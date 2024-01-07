ecea7aa8a3808cc52189f7caaa6d7f9def7d5311
fadyZar <93481957+fadyzar@users.noreply.github.com>
2024-01-07 07:41:39 +0200

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import usersRoutes from "./routes/usersRoutes.js";
+
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+import connectDB from "./config/db.js";
+dotenv.config();
+
+const app = express();
+
+app.use(cors()); // Solving cors
+app.use(express.json()); // Body parser middleware
+
+// User Routes - create user, get users , get single user
+app.use("/api/v1/users", usersRoutes);
+
+
+
+app.use(errorHandler); // Error handler middleware
+
+const PORT = process.env.PORT || 3000; // takes port from .env or just put 3000
+
+connectDB().then(()=>{
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
+})
